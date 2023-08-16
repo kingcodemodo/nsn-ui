c25192dceb7ef489082c28b1730cd15032457f0c
elmucholoco <mxdevelopment@yahoo.com>
2023-08-17 00:11:04 +0100

    Achieved Grid UI

--- app.js
+++ app.js
@@ -22,19 +22,37 @@ async function search(event){
 	displayUniversities(foundUniversities)
 }
 
+// const Home = ({ numbers }) => (
+// 	<div className="columns">
+// 	  {numbers.map((number) => (
+// 		<div className="column is-multiline is-12-mobile is-6-tablet is-4-desktop" key={number.id}>
+// 		 <div className="card">
+// 		  <div className="card-content">
+// 			<p className="title is-4">💠 {number.name}</p>
+// 		  </div>
+// 		 </div>
+// 	   </div>
+// 	 ))}
+//    </div>
+//  );
+
 function displayUniversities(foundUniversities){
 	document.getElementById("results").innerHTML = foundUniversities.map(formatUniversity).join("");
 }
 
 function formatUniversity(university){
 	return `
-		<div class="card p-2 my-2">
-			<div>${university.country}</div>
-			<div>${university.name}</div>
-			<div>${university.web_pages}</div>
-			<div><button class="button" onclick="saveUniversity('${university.name}')">Save</button></div>
-			<div><button class="button" onclick="deleteUniversity('${university.name}')">Delete</button></div>
+	<div class="column is-one-quarter-desktop is-full-mobile" key=${university.name}>
+		<div class="card px-5 mx-5 my-2 is-vcentered">
+			<div class="container p-2 m-2">
+				<div>${university.name}</div>
+				<div>${university.country}</div>
+				<div>${university.web_pages}</div>
+				<div><button class="button is-centered" onclick="saveUniversity('${university.name}')">Save</button></div>
+				<div><button class="button is-centered" onclick="deleteUniversity('${university.name}')">Delete</button></div>
+			</div>
 		</div>
+	</div>
 	`
 }
 
