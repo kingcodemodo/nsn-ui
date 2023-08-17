cf2f89d4d0391a5de78002a85a4973d1e80e8f24
elmucholoco <mxdevelopment@yahoo.com>
2023-08-17 16:07:00 +0100

    Updated The Card Styling With A More Post It Note General Styling To Match The University Context

--- app.js
+++ app.js
@@ -10,18 +10,21 @@ function checkSaved(){
 }
 
 async function search(event){
-
 	const searchTerm = event.target.value;
 
 	const universities = "http://universities.hipolabs.com/search?country=United+kingdom";
-	const results = await (await fetch(universities)).json()
 
-	const uniqueResults = (item, index, results) => results.indexOf(item) == index;
-	
-	const foundUniversities = results.filter(uniqueResults).filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
+	const results = await (await fetch(universities)).json()
+	const uniqueResults = results.filter(removeDuplicateUniversities)
+
+	const foundUniversities = uniqueResults.filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase()))
 	displayUniversities(foundUniversities)
 }
 
+function removeDuplicateUniversities(value, index, self) {
+	return index === self.findIndex(item => item.name === value.name);
+  }
+  
 function displayUniversities(foundUniversities){
 	document.getElementById("results").innerHTML = foundUniversities.map(formatUniversity).join("");
 }
@@ -29,7 +32,7 @@ function displayUniversities(foundUniversities){
 function formatUniversity(university){
 	return `
 	<div class="column is-one-quarter-desktop is-full-mobile" key=${university.name}>
-		<div class="card px-5 mx-5 my-2 is-vcentered">
+		<div class="card px-5 mx-5 my-2 is-vcentered" style="background-color:#FFFAAA">
 			<div class="container p-2 my-2">
 				<div><p class= "is-size-4 has-text-centered">${university.name}</p></div>
 				<div><p class= "is-size-6 has-text-centered">${university.country}</div>
@@ -51,15 +54,21 @@ function deleteUniversity(university){
 	checkSaved();
 }
 
-document.getElementById("header").innerHTML = "<div class='container p-4'><span class='has-text-weight-bold is-justify-content-center'>NSN</span></div>"
+document.getElementById("header").innerHTML = `
+<div class='container p-4 is-full'>
+	<span class='has-text-weight-bold is-justify-content-center'>
+		<p class = "is-size-1"><b> NSN <b></p>
+	</span>
+</div>
+`
 
 document.getElementById("footer").innerHTML = `
-<div class='container p-4 has-background-grey-dark has-text-white-ter'>
-NSN
+<div class='container p-4 has-background-grey-dark has-text-white-ter is-full'>
+<b> NSN <b>
 	<div class='is-pulled-right'>
-		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-house"> </i> Home</a>
-		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-eye"> </i> Privacy</a>
-		<a style="color:#FFFFFF;" href= /> <i class="fa-solid fa-book"> </i> Terms</a>
-		<a style="color:#FFFFFF;" href= /> <i class="fa-solid fa-message"></i> Contact</a>
+		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-house"> </i> <b> Home <b> </a>
+		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-eye"> </i> <b> Privacy <b> </a>
+		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-book"> </i> <b> Terms <b> </a>
+		<a style="color:#FFFFFF;" href=/> <i class="fa-solid fa-message"></i> <b> Contact <b> </a>
 	<div>
 </div>`
